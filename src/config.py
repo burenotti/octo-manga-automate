b2369655d89ccb42206e03b3dccffb23846cf3eb
Артём Буренин <burenotti@gmail.com>
2021-08-15 17:56:11 +0300

    В конфиг добавлен токен Telegra.ph API

--- src/config.py
+++ src/config.py
@@ -5,4 +5,6 @@ config = dotenv_values(".env")
 
 BOT_TOKEN = config.get("BOT_TOKEN")
 
+TELEGRAPH_TOKEN = config.get("TELEGRAPH_TOKEN")
+
 ADMINS_LIST = list(map(int, config.get("ADMINS_LIST").split(',')))
