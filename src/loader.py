bc8ecaa693dd5355016771c5aa116431d9df4df6
Артём Буренин <burenotti@gmail.com>
2021-08-15 18:10:09 +0300

    Обновлен loader

--- src/loader.py
+++ src/loader.py
@@ -1,9 +1,21 @@
+from aiohttp import ClientSession
+
+import config
+from backend.publisher import TelegraphPublisher
 from aiogram import Dispatcher, Bot, types
 from config import BOT_TOKEN
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from backend.driver import Driver
 
-bot = Bot(token=BOT_TOKEN)
+bot = Bot(token=BOT_TOKEN, parse_mode="html")
 
-storage = MemoryStorage()
+fsm_storage = MemoryStorage()
 
-dispatcher = Dispatcher(bot, storage=storage)
+local_storage = {}
+
+session = ClientSession()
+
+dispatcher = Dispatcher(bot, storage=fsm_storage)
+
+driver = Driver(session=session)
+publisher = TelegraphPublisher(access_token=config.TELEGRAPH_TOKEN)
