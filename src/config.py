5fcd92b1acb3b8e8b857dcf49369dbfc7045cec0
Артём Буренин <burenotti@gmail.com>
2021-08-22 00:37:20 +0300

    Добавил базу в конфиг

--- src/config.py
+++ src/config.py
@@ -13,3 +13,10 @@ ADMINS_LIST = list(map(int, config.get("ADMINS_LIST").split(',')))
 
 REDIS_HOST = config.get("REDIS_HOST")
 REDIS_PORT = config.get("REDIS_PORT")
+
+
+POSTGRES_HOST = config.get("POSTGRES_HOST")
+POSTGRES_PORT = config.get("POSTGRES_PORT")
+POSTGRES_USER = config.get("POSTGRES_USER")
+POSTGRES_PASSWORD = config.get("POSTGRES_PASSWORD")
+POSTGRES_DB = config.get("POSTGRES_DB")
